fc8a91174e4a41d082fd3a418e86405032b8e19b
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-02-10 20:14:43 +1030

    zaracl: move everything to zara-common
    move to zara-common :)
    
    Change-Id: I3f35e4dcc20edae1e4ca459db9fbc53d9ea98d61

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Copyright 2015 - The CyanogenMod Project
+
 # Device configuration for HTC Desire 601 (zaracl)
 
 ## Spec Sheet
--- cm.mk
+++ cm.mk
@@ -6,8 +6,5 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Device naming
 PRODUCT_DEVICE := zaracl
 PRODUCT_NAME := cm_zaracl
-PRODUCT_BRAND := htc
-PRODUCT_MODEL := Desire 601 CDMA
-PRODUCT_MANUFACTURER := HTC
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/sprint_wwe_vm/zaracl:4.4.2/KOT49H/332367.3:user/release-keys" PRIVATE_BUILD_DESC="2.15.652.3 CL332367 release-keys"
