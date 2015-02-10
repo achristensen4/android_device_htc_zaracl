38f3b319f45c827ce5230d621d122be09eec4123
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-02-10 15:00:55 +1030

    zaracl: cdma setup

--- cm.mk
+++ cm.mk
@@ -7,7 +7,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_DEVICE := zaracl
 PRODUCT_NAME := cm_zaracl
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := Desire 601
+PRODUCT_MODEL := Desire 601 CDMA
 PRODUCT_MANUFACTURER := HTC
 
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/htc_europe/zaracl:4.4.2/KOT49H/334166.10:user/release-keys" PRIVATE_BUILD_DESC="5.11.401.10 CL334166 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="htc/sprint_wwe_vm/zaracl:4.4.2/KOT49H/332367.3:user/release-keys" PRIVATE_BUILD_DESC="2.15.652.3 CL332367 release-keys"
